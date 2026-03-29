7a3c7761d3e8673ea06fe9ce3a07294682502641
Alina Volovychenko <kryvsun1901@gmail.com>
2026-03-29 19:54:11 +0300

    added simple calculator

---
+++ HW3.1.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+if operation == "+":
+    print(num1 + num2)
+
+elif operation == "-":
+    print(num1 - num2)
+
+elif operation == "*":
+    print(num1 * num2)
+
+elif operation == "/":
+    if num2 == 0:
+        print("Error")
+    else:
+        print(num1 / num2)
+
+else:
+    print("Unknown operation")
