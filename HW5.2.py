50a7b6f07ff95c6966c6344e88b8bc82d7923178
Alina Volovychenko <kryvsun1901@gmail.com>
2026-04-13 15:37:43 +0300

    long calculator

---
+++ HW5.2.py
@@ -0,0 +1,24 @@
+while True:
+    # Ввод данных
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operation (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if op == "+":
+        print(num1 + num2)
+    elif op == "-":
+        print(num1 - num2)
+    elif op == "*":
+        print(num1 * num2)
+    elif op == "/":
+        if num2 != 0:
+            print(num1 / num2)
+        else:
+            print("Error")
+    else:
+        print("Unknown operation")
+
+    answer = input("Wanna continue? (y/n or yes/no): ").lower()
+    if answer not in ("y", "yes"):
+        print("Calculator finished working")
+        break
